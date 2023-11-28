1af9dd14fc881df9655b5621cf8af9099988728e
Kryozen <masullo.simone00@gmail.com>
2023-11-28 16:17:23 +0200

    Fix links between html pages for vhost2

--- compose/vhost2/server.py
+++ compose/vhost2/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from urllib.request import urlopen
 import json
 
@@ -7,11 +7,24 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET'])
 def home():
     if(request.method == 'GET'):
-        f_html = open("/var/www/html/vhost2_diagnosis_input.php", "r")
-        return f_html.read()
+        return render_template("vhost2_main.html")
+
+@app.route('/diagnosis', methods = ['POST'])
+def diagnosis():
+     if(request.method == 'POST'):
+        return render_template("vhost2_diagnosis_input.html")
+
+@app.route('/visits', methods = ['POST'])
+def visits():
+     if(request.method == 'POST'):
+        return render_template("vhost2_show_visits.html")
+
+@app.route('/submit-diagnosis', methods = ['POST'])
+def submit_diagnosis():
+     if(request.method == 'POST'):
+        return render_template("vhost2_diagnosis_input.php")
 
 if __name__ == '__main__':
-
 	app.run(debug=True, host='0.0.0.0', port=80)
 
 

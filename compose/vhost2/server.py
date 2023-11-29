4d390b3e2972a07301baacb55c207edb1f0e32ce
Kryozen <masullo.simone00@gmail.com>
2023-11-29 17:10:06 +0200

    Fixing dynamically generated HTML pages using Flask render_template

--- compose/vhost2/server.py
+++ compose/vhost2/server.py
@@ -2,10 +2,14 @@ from flask import Flask, request, render_template, render_template_string
 import subprocess
 
 app = Flask(__name__)
+log_prefix = '|LOG MESSAGE|'
 
 @app.route('/', methods = ['GET'])
 def home():
     if(request.method == 'GET'):
+        log_message = '{} New request. Headers: {}'.format(log_prefix, request.headers)
+        print(log_message)
+
         return render_template("main.html")
 
 @app.route('/diagnosis', methods = ['POST'])
@@ -18,8 +22,20 @@ def visits():
      if(request.method == 'POST'):
         php_script_path = '/var/www/html/templates/show_visits.php'
         out = subprocess.check_output(['php', php_script_path], universal_newlines=True)
+        
+        log_msg = '{} PHP Script @{} ended with result {}.'.format(log_prefix, php_script_path, out)
+        print(log_msg)
+        print(type(out))
 
-        return render_template('show_visits.html', result= out)
+        doctor_surname = request.form.get("surname")
+
+        if (out is None or len(out) == 0):
+            out_html = 'No visits were found made by {}'.format(doctor_surname)
+        else:
+            log_msg = '{} Showing {} results.'.format(log_prefix, len(out))
+            out_html = out
+
+        return render_template('show_visits.html', html_response= out_html, doctor= doctor_surname)
 
 @app.route('/submit-diagnosis', methods = ['POST'])
 def submit_diagnosis():
@@ -27,7 +43,10 @@ def submit_diagnosis():
         php_script_path = '/var/www/html/templates/diagnosis_input.php'
 
         out = subprocess.check_output(['php', php_script_path], universal_newlines=True)
-
+        
+        log_msg = '{} PHP Script @{} ended with result {}.'.format(log_prefix, php_script_path, out)
+        print(log_msg)
+        
         return render_template('diagnosis_input.html', result= out)
 
         return render_template_string("{{ php_output }}", php_output= out)

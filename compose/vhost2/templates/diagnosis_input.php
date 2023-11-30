b4f447df7df44802b1a68448ced2f5b41164a601
Kryozen <masullo.simone00@gmail.com>
2023-11-30 15:52:29 +0200

    Bug fix: adding template folder for vhost2

---
+++ compose/vhost2/templates/diagnosis_input.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="https://fonts.googleapis.com/css?family=Karla:700 subset=latin,latin-ext,cyrillic" rel="stylesheet">
+<link rel="stylesheet" href="../styles.css">
+<title>Додавання даних</title>
+</head>
+
+<body>
+<?php
+// Assuming you have a MySQL database connection
+$servername = "database";
+$username = "root";
+$password = "root";
+$dbname = "hospital_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$surname = $_POST['surname'];
+$diagnosis = $_POST['diagnosis'];
+$appointmentDate = $_POST['appointmentDate'];
+
+// Insert data into MySQL database
+$sql = "INSERT INTO medical_records (surname, diagnosis, appointment_date) VALUES ('$surname', '$diagnosis', '$appointmentDate')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record added successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the connection
+$conn->close();
+?>
+<a type="button" id="back" href='vhost2_main.html'><button>Main page</button></a>
+</body>
+</html>
---
+++ compose/vhost2/templates/show_visits.php
@@ -0,0 +1,27 @@
+<?php
+// Assuming you have a MySQL database connection
+$servername = "database";
+$username = "root";
+$password = "root";
+$dbname = "hospital_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+// Get the doctor's name from the form
+$doctorName = $_GET['doctor'];
+
+// Fetch data from the database for the specific doctor
+$sql = "SELECT * FROM appointments WHERE doctor = '$doctorName'"; 
+// ATTENTION: THIS IS SUBJECT TO SQL INJECTION (we need to avoid passing input parameters like this in a query)
+
+$result = $conn->query($sql);
+
+return $result
+?>

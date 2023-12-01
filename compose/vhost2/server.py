89625774dc69d9e7ba088850d2b685bc76081657
Kryozen <masullo.simone00@gmail.com>
2023-12-01 17:03:03 +0200

    Modified app logic for vhost2

--- compose/vhost2/server.py
+++ compose/vhost2/server.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, render_template, render_template_string
+import mysql.connector
 import subprocess
 
 app = Flask(__name__)
+
 log_prefix = '|LOG MESSAGE|'
+db_host, user, password, db_name = 'database', 'root', 'root', 'hospital_db'
+
 
 @app.route('/', methods = ['GET'])
 def home():
@@ -19,15 +23,34 @@ def diagnosis():
 
 @app.route('/visits', methods = ['POST'])
 def visits():
-     if(request.method == 'POST'):
-        php_script_path = '/var/www/html/templates/show_visits.php'
-        out = subprocess.check_output(['php', php_script_path], universal_newlines=True)
-        
-        log_msg = '{} PHP Script @{} ended with result {}.'.format(log_prefix, php_script_path, out)
-        print(log_msg)
-        print(type(out))
+    if(request.method == 'POST'):
+        # php_script_path = '/var/www/html/templates/show_visits.php'
+        # php_script_exec = subprocess.Popen(['php', php_script_path], shell=True, stdout=subprocess.PIPE)
 
+        # print('{} PHP Script ended: {}'.format(log_prefix, php_script_exec))
+        # out = php_script_exec.stdout.read()
+
+        # log_msg = '{} PHP Script @{} ended with result {}.'.format(log_prefix, php_script_path, out)
+        # print(log_msg)
+        # print(type(out))
+        
+        # Get parameters from input form
         doctor_surname = request.form.get("surname")
+
+        # Start database connection
+        con = mysql.connector.connect(db_host, user, password, db_name)
+        cursor = con.cursor()
+
+        # Define sql query
+        sql_query = 'SELECT * \
+            FROM (Visit JOIN Doctor ON Visit.doctor = Doctor.id) \
+            WHERE Doctor.last_name = "{}";'.format(doctor_surname)
+        
+        # Run sql query
+        cursor.execute(sql_query)
+
+        # Fetch results
+        out = cursor.fetchall()
 
         if (out is None or len(out) == 0):
             out_html = 'No visits were found made by {}'.format(doctor_surname)
@@ -53,5 +76,4 @@ def submit_diagnosis():
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=80)
-
 
